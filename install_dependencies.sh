e99bdb56e4620e52eca8a1f240027ef60a6c62f9
ashutoshs25 <65936277+ashutoshs25@users.noreply.github.com>
2024-03-31 15:33:41 -0400

    Minor updates to make usable on Fabric

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # System packages
-apt-get install texlive-latex-recommended cm-super dvipng
+apt-get -y install texlive-latex-recommended cm-super dvipng
 
 # Python3 packages
 pip3 install matplotlib pandas pyroute2 colour
